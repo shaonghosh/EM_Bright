b2810b5884e8e915143d71bbb5b225b76d6f6adb
shaonghosh <shaonghosh@gmail.com>
2016-09-18 10:19:24 +0200

    Update sourceClassDriver.py

--- sourceClassDriver.py
+++ sourceClassDriver.py
@@ -71,90 +71,89 @@ alert_type = 'None'
 if streamdata['alert_type']:
     alert_type = streamdata['alert_type']
 
-#if alert_type == 'new':
-    # The object is a serialized event. Get the FAR
+print alert_type
+if alert_type == 'new':
+    log = open('logFile.txt', 'a')
+    graceid = str(streamdata['uid'])
+    log.writelines('\n' + str(datetime.datetime.today()) + '\t' + 'Analyzing event: ' + graceid + '\n')
+    coinc_path = 'all_coincs' ### Currently hardcoded
+    psd_path = 'all_psds' ### Currently hardcoded
+    source_class_path = 'all_source_classifications' ### Currently hardcoded
+    try:
+        os.system('mkdir -p ' + coinc_path)
+        log.writelines(str(datetime.datetime.today()) + '\t' + 'Successfully created coinc directory\n')
 
-log = open('logFile.txt', 'a')
-graceid = str(streamdata['uid'])
-log.writelines('\n' + str(datetime.datetime.today()) + '\t' + 'Analyzing event: ' + graceid + '\n')
-coinc_path = 'all_coincs' ### Currently hardcoded
-psd_path = 'all_psds' ### Currently hardcoded
-source_class_path = 'all_source_classifications' ### Currently hardcoded
-try:
-    os.system('mkdir -p ' + coinc_path)
-    log.writelines(str(datetime.datetime.today()) + '\t' + 'Successfully created coinc directory\n')
-
-    os.system('mkdir -p ' + psd_path)
-    log.writelines(str(datetime.datetime.today()) + '\t' + 'Successfully created psd directory\n')
+        os.system('mkdir -p ' + psd_path)
+        log.writelines(str(datetime.datetime.today()) + '\t' + 'Successfully created psd directory\n')
     
-    os.system('mkdir -p ' + source_class_path)
-    log.writelines(str(datetime.datetime.today()) + '\t' + 'Successfully created results directory\n')
+        os.system('mkdir -p ' + source_class_path)
+        log.writelines(str(datetime.datetime.today()) + '\t' + 'Successfully created results directory\n')
 
-except:
-    log.writelines(str(datetime.datetime.today()) + '\t' + 'Failed to creat coinc and/or psd and/or results directory, Check write privilege to the given path\n')
-    exit(1)
-    
-
-x = 1
-### WARNING! This can cause an infinite loop. Discuss this with the reviewers
-countTrials = 0
-while x == 1:
-    x = getCoinc(graceid, coinc_path, psd_path)
-    if countTrials >= 5:
-        log.writelines(str(datetime.datetime.today()) + '\t' + 'Could not fetch coinc and/or psd files\n')
+    except:
+        log.writelines(str(datetime.datetime.today()) + '\t' + 'Failed to creat coinc and/or psd and/or results directory, Check write privilege to the given path\n')
         exit(1)
-    if x == 1: Time.sleep(5) ### Wait for five seconds if getCoinc is unsuccessful
-    countTrials += 1
+    
 
-log.writelines(str(datetime.datetime.today()) + '\t' + 'Successfully fetched coinc and/or psd files\n')
+    x = 1
+    ### WARNING! This can cause an infinite loop. Discuss this with the reviewers
+    countTrials = 0
+    while x == 1:
+        x = getCoinc(graceid, coinc_path, psd_path)
+        if countTrials >= 5:
+            log.writelines(str(datetime.datetime.today()) + '\t' + 'Could not fetch coinc and/or psd files\n')
+            exit(1)
+        if x == 1: Time.sleep(5) ### Wait for five seconds if getCoinc is unsuccessful
+        countTrials += 1
 
-
-### Check if this event  has been analyzed ###
-if os.path.isfile(source_class_path + '/Source_Classification_' + graceid + '_.dat'):
-    log.writelines(str(datetime.datetime.today()) + '\t' + 'Event already analyzed... skipping\n')
-    exit(0)
-
-
-### Check if the required coinc file and the psd file is present ###
-if os.path.isfile(coinc_path + '/coinc_' + graceid + '.xml') and os.path.isfile(psd_path + '/psd_' + graceid + '.xml.gz'):
-    start = Time.time()
-    coincFileName = [coinc_path + '/coinc_' + graceid + '.xml']
-    [mass1, mass2, chi1, time] = readCoinc(coincFileName)
-
-else:
-    print 'Did not find the coinc and psd files... quitting...'
-    exit(1)
+    log.writelines(str(datetime.datetime.today()) + '\t' + 'Successfully fetched coinc and/or psd files\n')
 
 
-File = open(coinc_path + '/masses_chi1_' + graceid + '_.dat', 'w')
-File.writelines(graceid + '\t' + str(mass1) + '\t' +  str(mass2) + '\t' + str(chi1) + '\n')
+    ### Check if this event  has been analyzed ###
+    if os.path.isfile(source_class_path + '/Source_Classification_' + graceid + '_.dat'):
+        log.writelines(str(datetime.datetime.today()) + '\t' + 'Event already analyzed... skipping\n')
+        exit(0)
 
-File.close()
 
-samples_sngl = getSamples(graceid, mass1, mass2, chi1, 1000, {'H1=' + psd_path + '/psd_' + graceid + '.xml.gz'}, {'L1=' + psd_path + '/psd_' + graceid + '.xml.gz'}, saveData=True)
-if ~np.any( np.isnan(samples_sngl[0]) ):
-    diskMassObject_sngl = genDiskMassProbability.genDiskMass(samples_sngl, 'test', 0.03)
-    [NS_prob_1_sngl, NS_prob_2_sngl, diskMass_sngl] = diskMassObject_sngl.fromEllipsoidSample()
-    em_bright_prob_sngl = np.sum((diskMass_sngl > 0.)*100./len(diskMass_sngl))
+    ### Check if the required coinc file and the psd file is present ###
+    if os.path.isfile(coinc_path + '/coinc_' + graceid + '.xml') and os.path.isfile(psd_path + '/psd_' + graceid + '.xml.gz'):
+        start = Time.time()
+        coincFileName = [coinc_path + '/coinc_' + graceid + '.xml']
+        [mass1, mass2, chi1, time] = readCoinc(coincFileName)
 
-else:
-    print 'Return was NaNs'
-    log.writelines(str(datetime.datetime.today()) + '\t' + 'Return was NaNs\n')
-    [NS_prob_2_sngl, em_bright_prob_sngl] = [0., 0.]
+    else:
+        print 'Did not find the coinc and psd files... quitting...'
+        exit(1)
 
-end = Time.time()
-log.writelines(str(datetime.datetime.today()) + '\t' + 'Time taken in computing probabilities = ' + str(end - start) + '\n')
 
-source_classification = open(source_class_path + '/Source_Classification_' + graceid + '_.dat', 'w')
-source_classification.writelines('The probability of second object being a neutron star for the trigger ' + graceid + ' = ' + str(NS_prob_2_sngl) + '\n')
-source_classification.writelines('The probability of remnant mass in the disk in excess of 0.03 M_sun for the trigger ' + graceid + ' = '  + str(NS_prob_2_sngl) + '\n')
+    File = open(coinc_path + '/masses_chi1_' + graceid + '_.dat', 'w')
+    File.writelines(graceid + '\t' + str(mass1) + '\t' +  str(mass2) + '\t' + str(chi1) + '\n')
 
-source_classification.close()
+    File.close()
 
-message = 'Computed from detection pipeline: The probability of second object being a neutron star  = ' + str(NS_prob_2_sngl) + '\n The probability of remnant mass in the disk in excess of 0.03 M_sun = '  + str(NS_prob_2_sngl) + '\n'
+    samples_sngl = getSamples(graceid, mass1, mass2, chi1, 1000, {'H1=' + psd_path + '/psd_' + graceid + '.xml.gz'}, {'L1=' + psd_path + '/psd_' + graceid + '.xml.gz'}, saveData=True)
+    if ~np.any( np.isnan(samples_sngl[0]) ):
+        diskMassObject_sngl = genDiskMassProbability.genDiskMass(samples_sngl, 'test', 0.03)
+        [NS_prob_1_sngl, NS_prob_2_sngl, diskMass_sngl] = diskMassObject_sngl.fromEllipsoidSample()
+        em_bright_prob_sngl = np.sum((diskMass_sngl > 0.)*100./len(diskMass_sngl))
 
-gdb.writeLog(graceid, message)
-gdb.writeLog(graceid, message, tagname='em_follow')
+    else:
+        print 'Return was NaNs'
+        log.writelines(str(datetime.datetime.today()) + '\t' + 'Return was NaNs\n')
+        [NS_prob_2_sngl, em_bright_prob_sngl] = [0., 0.]
+
+    end = Time.time()
+    log.writelines(str(datetime.datetime.today()) + '\t' + 'Time taken in computing probabilities = ' + str(end - start) + '\n')
+
+    source_classification = open(source_class_path + '/Source_Classification_' + graceid + '_.dat', 'w')
+    source_classification.writelines('The probability of second object being a neutron star for the trigger ' + graceid + ' = ' + str(NS_prob_2_sngl) + '\n')
+    source_classification.writelines('The probability of remnant mass in the disk in excess of 0.03 M_sun for the trigger ' + graceid + ' = '  + str(NS_prob_2_sngl) + '\n')
+
+    source_classification.close()
+
+    message = 'Computed from detection pipeline: The probability of second object being a neutron star  = ' + str(NS_prob_2_sngl) + '\n The probability of remnant mass in the disk in excess of 0.03 M_sun = '  + str(NS_prob_2_sngl) + '\n'
+
+    gdb.writeLog(graceid, message)
+    gdb.writeLog(graceid, message, tagname='em_follow')
 
 
 
