04fef147f7c6cef37da6d6d41d02dc327f0c9800
shaonghosh <shaonghosh@gmail.com>
2016-09-21 10:54:53 +0530

    Update initiate.py
    Adapted to new LVAlert where the password is read from the .netrc file.

--- initiate.py
+++ initiate.py
@@ -4,9 +4,7 @@ import optparse
 
 parser = optparse.OptionParser()
 parser.add_option("-U", "--user", action="store", type="string", metavar="NAME", help="username albert.einstein")
-### This will be removed for the new LVAlert ###
-parser.add_option("-P", "--password", action="store", type="string", metavar="NAME", help="lv alert password")
-
+parser.add_option("-N", "--netrcpath", action="store", type="string", metavar="NAME", help="path to the .netrc file")
 (opts,args) = parser.parse_args()
 
 
@@ -28,8 +26,7 @@ iniFile.writelines(text)
 iniFile.close()
 
 os.system('chmod 777 sourceClassDriver.py')
-lvalert_cmd = 'lvalert_listen -a ' + opts.user + ' -b ' + opts.password + ' -c ' + work_dir + '/myLVAlertListen.ini -r alertInstance &'
-
+lvalert_cmd = 'lvalert_listen --user ' + opts.user + ' --netrc ' + opts.netrcpath + ' -c ' + work_dir +  '/myLVAlertListen.ini -r alertInstance &'
 print 'Launching LValert job...'
 os.system(lvalert_cmd)
 print 'Done...'
