59c4e45805fb4674dbfeedaa8fcd5f0f723eb570
shaonghosh <shaonghosh@gmail.com>
2016-09-26 13:00:43 +0530

    Update initiate.py
    initiate.py changed such that it does not create the LVAlert ini file.

--- initiate.py
+++ initiate.py
@@ -7,23 +7,7 @@ parser.add_option("-U", "--user", action="store", type="string", metavar="NAME",
 parser.add_option("-N", "--netrcpath", action="store", type="string", metavar="NAME", help="path to the .netrc file")
 (opts,args) = parser.parse_args()
 
-
 work_dir = os.getcwd()
-
-text = '''[cbc_gstlal_mdc]
-nodes = test_gstlal
-executable = ''' + work_dir + '''/sourceClassDriver.py
-
-[cbc_gstlal_highmass]
-nodes = test_gstlal
-executable = ''' + work_dir + '''/sourceClassDriver.py
-
-'''
-
-iniFile = open('myLVAlertListen.ini', 'w')
-
-iniFile.writelines(text)
-iniFile.close()
 
 os.system('chmod 777 sourceClassDriver.py')
 lvalert_cmd = 'lvalert_listen --user ' + opts.user + ' --netrc ' + opts.netrcpath + ' -c ' + work_dir +  '/myLVAlertListen.ini -r alertInstance &'
